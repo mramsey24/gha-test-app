85b8d5b5dac39bcb63ccdccaa5a727091ea75df7
ramseym4 <ramseym4@nationwide.com>
2022-05-23 14:41:59 -0400

    Correct package names in tests

--- src/test/java/com/mvrsoftware/springboot/GhaTestAppApplicationTests.java
+++ src/test/java/com/mvrsoftware/springboot/GhaTestAppApplicationTests.java
@@ -1,10 +1,10 @@
-package com.mvrsoftware.ghatestapp;
+package com.mvrsoftware.springboot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GhaTestAppApplicationTests {
+class GhaTestAppTests {
 
 	@Test
 	void contextLoads() {
